814ce54c9cb5f1d750928f793866a30881af8738
priya <priyapalanisamy235@gmail.com>
2025-05-16 16:01:10 +0530

    day3

---
+++ day 3/script.js
@@ -0,0 +1,25 @@
+var a=10;
+var b=30;
+
+console.log(a)
+console.log(b)
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+ 
+function add(){
+    var a=10;
+    var b=20;
+    console.log(a+b);
+}
+add()
+function add(a,b){
+
+   console.log(a+b)
+}
+add(50,30)
+add(100,30)
+add(10,30)
