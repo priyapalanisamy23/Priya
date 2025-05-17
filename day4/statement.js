889ae813204fc5784576f66edf5c2669f3e3cb1f
priya <priyapalanisamy235@gmail.com>
2025-05-17 15:52:27 +0530

    day4

---
+++ day4/statement.js
@@ -0,0 +1,70 @@
+// n=10
+// if (n %2 == 0){
+//   console.log("even")
+//}
+// else{
+//  console.log("odd")
+//}
+
+// n=1;
+// while (n <= 10) {
+//   if (n % 2 == 0) {
+//      console.log(`${n} is even`)
+//   }
+//   else {
+//      console.log(`${n} is odd`)
+//   }
+//   n++;
+//   }
+//
+// n = 1;
+//do {
+//   if (n % 2 == 0){
+//      console.log(`${n} is even`)
+//  }
+//  else {
+//      console.log(`${n} is odd`)
+//  }
+//  n++;} while (n <= 10);
+//
+//n=7
+//switch (n){
+//    case 1:
+//        console.log("Sunday")
+//        break;
+//   case 2:
+//        console.log("Monday")
+//        break;
+//    case 3:
+//        console.log("Tuesday")
+//        break;
+//    case 4:
+//        console.log("Wednesday")
+//        break;
+//    case 5:
+//        console.log("Thuesday")
+//        break;
+//    case 6:
+//        console.log("Friday")
+//        break;
+//    case 7:
+//        console.log("Saturday")
+//        break;
+//    default:
+//        console.log("Enter the valid number")
+//}
+
+//const sum = () => {
+//   console.log(10 + 20)
+//}
+//sum()
+//
+//var element = document.createElement("div")
+//element.setAttribute("class","main")
+//element.innerText = "this is element from dom"
+//document.body.append(element);
+
+
+n=10
+var result = (n%2==0)?"even": "odd"
+console.log(result)
